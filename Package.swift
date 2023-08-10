d3c704558aac09fbde547ea2328e5b2892f684b5
wyland <wyland@fiscalnote.com>
2023-08-10 11:40:21 -0400

    Update Package.swift
    bumping dependency

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         ],
     dependencies: [
         .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
-        .package(url: "https://github.com/apocolipse/Thrift-Swift.git", from: "1.1.0"),
+        .package(url: "https://github.com/apocolipse/Thrift-Swift.git", from: "1.2.0"),
     ],
     targets: [
         .target(
